991e4107e95b78536885683551e1ba43b26c597f
eijirou <sacjapan@outlook.com>
2026-03-13 15:51:40 +0900

    StartProject

---
+++ SwiftCoins/SwiftCoins/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftCoins
+//
+//  Created by 野澤英二郎 on 2026/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftCoins/SwiftCoins/SwiftCoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinsApp.swift
+//  SwiftCoins
+//
+//  Created by 野澤英二郎 on 2026/03/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
